1b6e74385f242d4c206cd77a00e8a93d1d7a1c27
David Brodie <david.brodie122@gmail.com>
2021-01-13 19:47:34 -0500

    Working state

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,38 @@
-import React, { useState, useEffect } from "react";
-import Searchbar from "./Searchbar";
-import { getLinks } from "../api";
-import SearchResults from "./SearchResults";
+import React, { useState, useEffect } from 'react';
+import Searchbar from './Searchbar';
+import { getLinks } from '../api';
+import SearchResults from './SearchResults';
 
 const App = () => {
-  const [links, setLinks] = useState("");
+  const [links, setLinks] = useState('');
 
   useEffect(() => {
     getLinks()
-      .then((response) => {
+      .then(response => {
         console.log(response);
         setLinks([]);
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error.message);
       });
   }, []);
 
+  function handleClick() {
+    fetch('/potato')
+      .then(res => {
+        console.log('the res', res);
+        return res.json();
+      })
+      .then(data => console.log('the data', data))
+      .catch(console.error);
+  }
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Search for links</h1>
       <Searchbar setResults={setLinks}></Searchbar>
       <SearchResults results={links} />
+      <button onClick={handleClick}> Server Test</button>
     </div>
   );
 };
